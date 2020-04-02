7b0ca0c27f3572fbd1d02b5fc7633a932e808508
Giulio Fagioli <fagioli.giulio@gmail.com>
2020-04-02 11:34:23 +0200

    Implement findAll method, and fromMapToAccounts private methods

--- src/main/java/dev/justgiulio/passwordmanager/repository/AccountRedisRepository.java
+++ src/main/java/dev/justgiulio/passwordmanager/repository/AccountRedisRepository.java
@@ -2,17 +2,39 @@ package dev.justgiulio.passwordmanager.repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import dev.justgiulio.passwordmanager.model.Account;
+import dev.justgiulio.passwordmanager.model.Credential;
 import redis.clients.jedis.Jedis;
 
 public class AccountRedisRepository {
 
-	public AccountRedisRepository(Jedis jedis) {
-		// TODO Auto-generated constructor stub
+	Jedis client;
+
+	public AccountRedisRepository(Jedis client) {
+		this.client = client;
 	}
 
-	public List findAll() {
-		return new ArrayList<>();
+	public List<Account> findAll() {
+		List<String> keys;
+		List<Account> accounts = new ArrayList<>();
+		keys = client.keys("*").stream().collect(Collectors.toList());
+		keys
+			.stream()
+			.map(key -> this.fromMapToAccount(key,this.client.hgetAll(key)))
+			.forEach(account -> accounts.addAll(account));
+		return accounts;
+	}
+
+	private List<Account> fromMapToAccounts(String key, Map<String, String> jedisSavedMap) {
+		List<String> savedKeys;
+		List<Account> savedAccounts;
+		savedKeys = jedisSavedMap.keySet().stream().collect(Collectors.toList());
+		savedAccounts = savedKeys.stream().map(keyMap -> new Account(key,new Credential(key, jedisSavedMap.get(key))))
+				.collect(Collectors.toList());
+		return savedAccounts;
 	}
 
 }
