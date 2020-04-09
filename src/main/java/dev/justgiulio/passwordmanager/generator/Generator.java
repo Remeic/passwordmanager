d4ba548646c06e96f3f17da470a254d8e3ca4eed
Giulio Fagioli <fagioli.giulio@gmail.com>
2020-04-09 11:01:50 +0200

    Implement minimum code for generate, exception with length lower than 1

--- src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
+++ src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
@@ -2,4 +2,10 @@ package dev.justgiulio.passwordmanager.generator;
 
 public class Generator {
 
+	public void generate(int length) {
+		if(length < 1) {
+			throw new IllegalArgumentException("Length value must be greater than 0");
+		}
+	}
+
 }
