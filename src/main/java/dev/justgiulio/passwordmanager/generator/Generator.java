3ab702de5c074e335209b7d3f5183c1cee8f3fe7
Giulio Fagioli <fagioli.giulio@gmail.com>
2020-04-09 11:29:10 +0200

    Implement minimum code for strength greater equal to 0

--- src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
+++ src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
@@ -2,9 +2,12 @@ package dev.justgiulio.passwordmanager.generator;
 
 public class Generator {
 
-	public void generate(int length) {
+	public void generate(int length, int strength) {
 		if(length < 1 || length > 32) {
 			throw new IllegalArgumentException("Length value must be beetween than 1 and 32");
+		}
+		if(strength < 0) {
+			throw new IllegalArgumentException("Length value must be greater equal to 0");
 		}
 	}
 
