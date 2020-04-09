799477fcf61685db243e818705bdb2a04cec57db
Giulio Fagioli <fagioli.giulio@gmail.com>
2020-04-09 11:31:31 +0200

    Implement new boundary condition to generate for strength value, must be between 0 and 2

--- src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
+++ src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
@@ -6,8 +6,8 @@ public class Generator {
 		if(length < 1 || length > 32) {
 			throw new IllegalArgumentException("Length value must be beetween than 1 and 32");
 		}
-		if(strength < 0) {
-			throw new IllegalArgumentException("Length value must be greater equal to 0");
+		if(strength < 0 || strength > 2) {
+			throw new IllegalArgumentException("Length value must be beetween than 0 and 2");
 		}
 	}
 
