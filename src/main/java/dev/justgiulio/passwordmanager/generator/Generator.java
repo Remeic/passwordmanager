1958ac171bbc2cae07db1f4a993a01f5c261bf6b
Giulio Fagioli <fagioli.giulio@gmail.com>
2020-04-09 19:26:35 +0200

    Add minimum code for fixed length of password, one character for now

--- src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
+++ src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
@@ -1,14 +1,24 @@
 package dev.justgiulio.passwordmanager.generator;
 
 public class Generator {
+	
+	
+	public Generator() {
+		super();
+	}
 
-	public void generate(int length, int strength) {
+	public String generate(int length, int strength) {
 		if(length < 1 || length > 32) {
 			throw new IllegalArgumentException("Length value must be beetween than 1 and 32");
 		}
 		if(strength < 0 || strength > 2) {
 			throw new IllegalArgumentException("Length value must be beetween than 0 and 2");
 		}
+		StringBuilder result = new StringBuilder(length);
+		for (int i = 0; i < length; i++) {
+			result.append("g");
+		}
+		return result.toString();
 	}
 
 }
