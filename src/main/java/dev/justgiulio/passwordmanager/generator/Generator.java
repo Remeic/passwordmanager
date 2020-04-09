fc1eac4ed7c9ded878941193f5c739e3837b0627
Giulio Fagioli <fagioli.giulio@gmail.com>
2020-04-09 11:07:11 +0200

    Add check for boundary value of length on generate method

--- src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
+++ src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
@@ -3,8 +3,8 @@ package dev.justgiulio.passwordmanager.generator;
 public class Generator {
 
 	public void generate(int length) {
-		if(length < 1) {
-			throw new IllegalArgumentException("Length value must be greater than 0");
+		if(length < 1 || length > 32) {
+			throw new IllegalArgumentException("Length value must be beetween than 1 and 32");
 		}
 	}
 
