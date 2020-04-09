645cb21c4a3b0b3d15821207b6b489b7768a6fd8
Giulio Fagioli <fagioli.giulio@gmail.com>
2020-04-09 10:52:38 +0200

    Create  generator junit test file

---
+++ src/test/java/dev/justgiulio/passwordmanager/generator/GeneratorTest.java
@@ -0,0 +1,23 @@
+package dev.justgiulio.passwordmanager.generator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.giuliofagioli.passwordgenerator.PasswordGenerator;
+
+public class GeneratorTest {
+
+	Generator generator;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
