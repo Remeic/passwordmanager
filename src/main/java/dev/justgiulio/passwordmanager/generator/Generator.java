c6a6163bb0b6a639c874b521c2842a35e3bc5a8b
Giulio Fagioli <fagioli.giulio@gmail.com>
2020-04-14 11:52:41 +0200

    Implement code for generating password with random char from dictionary

--- src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
+++ src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
@@ -1,11 +1,26 @@
 package dev.justgiulio.passwordmanager.generator;
 
+import java.security.SecureRandom;
+
 public class Generator {
 	
+	private String dictionary;
+	private SecureRandom randomizer;
 	
 	public Generator() {
 		super();
+		this.dictionary = "abcdefghijklmnopqrstuvwxyz";
+		setRandomizer(new SecureRandom());
 	}
+	
+	
+
+	public Generator(String dictionary) {
+		super();
+		this.dictionary = dictionary;
+	}
+
+
 
 	public String generate(int length, int strength) {
 		if(length < 1 || length > 32) {
@@ -15,10 +30,21 @@ public class Generator {
 			throw new IllegalArgumentException("Length value must be beetween than 0 and 2");
 		}
 		StringBuilder result = new StringBuilder(length);
+		int randomCharPosition;
 		for (int i = 0; i < length; i++) {
-			result.append("g");
+			randomCharPosition = randomizer.nextInt(this.dictionary.length());
+			result.append(dictionary.charAt(randomCharPosition));
 		}
 		return result.toString();
 	}
+
+
+
+	public void setRandomizer(SecureRandom randomizer) {
+		this.randomizer = randomizer;
+	}
+	
+	
+
 
 }
