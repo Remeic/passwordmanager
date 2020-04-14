a48004a15a3e97fef38d0ccf43fee32e9d42be5b
Giulio Fagioli <fagioli.giulio@gmail.com>
2020-04-14 19:33:15 +0200

    Add numbers to dictionary based to selected strenght

--- src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
+++ src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
@@ -4,36 +4,34 @@ import java.security.SecureRandom;
 
 public class Generator {
 	
-	private String dictionary;
+	private String dictionaryLetters;
+	private String dictionaryNumbers;
 	private SecureRandom randomizer;
 	
 	public Generator() {
 		super();
-		this.dictionary = "abcdefghijklmnopqrstuvwxyz";
+		this.dictionaryLetters = "abcdefghijklmnopqrstuvwxyz";
+		this.dictionaryNumbers = "0123456789";
 		setRandomizer(new SecureRandom());
 	}
 	
-	
-
-	public Generator(String dictionary) {
-		super();
-		this.dictionary = dictionary;
-	}
-
-
 
 	public String generate(int length, int strength) {
+		String choosedDictionary = this.dictionaryLetters;
 		if(length < 1 || length > 32) {
 			throw new IllegalArgumentException("Length value must be beetween than 1 and 32");
 		}
 		if(strength < 0 || strength > 2) {
 			throw new IllegalArgumentException("Length value must be beetween than 0 and 2");
 		}
+		if(strength == 1) {
+			choosedDictionary += this.dictionaryNumbers;
+		}
 		StringBuilder result = new StringBuilder(length);
 		int randomCharPosition;
 		for (int i = 0; i < length; i++) {
-			randomCharPosition = randomizer.nextInt(this.dictionary.length());
-			result.append(dictionary.charAt(randomCharPosition));
+			randomCharPosition = randomizer.nextInt(choosedDictionary.length());
+			result.append(choosedDictionary.charAt(randomCharPosition));
 		}
 		return result.toString();
 	}
