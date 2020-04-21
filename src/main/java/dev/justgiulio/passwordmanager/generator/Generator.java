2da03297bcfa7c7438f639fe8c852f489188901e
Giulio Fagioli <fagioli.giulio@gmail.com>
2020-04-21 09:25:22 +0200

    Implement secure regeneration of password, if it not match criteria

--- src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
+++ src/main/java/dev/justgiulio/passwordmanager/generator/Generator.java
@@ -1,7 +1,11 @@
 package dev.justgiulio.passwordmanager.generator;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Generator {
 
@@ -11,7 +15,7 @@ public class Generator {
 	private static final int LOW_STRENGTH = 0;
 	private static final int MEDIUM_STRENGTH = 1;
 	private static final int HIGH_STRENGTH = 2;
-
+	
 
 	private SecureRandom randomizer;
 
@@ -27,11 +31,15 @@ public class Generator {
 		if (strength < 0 || strength > 2) {
 			throw new IllegalArgumentException("Length value must be beetween than 0 and 2");
 		}
-		return randomizer.ints(length, 0, this.getDictionary(strength).length())
-				.mapToObj(letterNumber -> this.fromCharToString(this.getDictionary(strength), letterNumber)).collect(Collectors.joining());
+		String password;
+		do {
+			password = getPassword(length, this.getDictionary(strength));
+		} while (!isValidPassword(password,strength));
+		return password;
 
 	}
 
+	
 	public void setRandomizer(SecureRandom randomizer) {
 		this.randomizer = randomizer;
 	}
@@ -39,7 +47,41 @@ public class Generator {
 	/**
 	 * Private methods
 	 */
+	
+	private boolean isValidPassword(String password, int strength) {
+		boolean result = false;
+		if(strength == LOW_STRENGTH) {
+			result = true;
+		}
+		else if(strength == MEDIUM_STRENGTH) {
+			result = isCharContained(password, DICTIONARY_LETTERS) && isCharContained(password,DICTIONARY_NUMBERS);
+		}
+		else if(strength == HIGH_STRENGTH) {
+			result = isCharContained(password, DICTIONARY_LETTERS) && isCharContained(password,DICTIONARY_NUMBERS) && isCharContained(password,DICTIONARY_SYMBOLS);
+		}
+		return result;
+	}
+	
+	private boolean isCharContained(String password, String dictionary) {
+		boolean result  = false;
+		for(char character:dictionary.toCharArray()) {
+			if(password.indexOf(character) != -1) {
+				result = true;
+			}
+		}
+		return result;
+	}
 
+	
+	private String getPassword(int length, String dictionary) {
+		IntStream intStream =  randomizer.ints(length, 14, dictionary.length());
+		StringBuilder result = new StringBuilder();
+		for(int charPosition:intStream.toArray()) {
+			result.append(Character.toString(dictionary.charAt(charPosition)));
+		}
+		return result.toString();
+	}
+	
 	private String getDictionary(int strength) {
 		String resultDictionary = DICTIONARY_LETTERS;
 		if(strength == MEDIUM_STRENGTH) {
@@ -52,8 +94,5 @@ public class Generator {
 		return resultDictionary;
 	}
 
-	private String fromCharToString(String dictionary, int position) {
-		return Character.toString(dictionary.charAt(position));
-	}
 
 }
