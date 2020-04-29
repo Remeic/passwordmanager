cb399faaa1ec8d4beac8b57a906f6b3e971378bd
Giulio Fagioli <fagioli.giulio@gmail.com>
2020-04-29 17:51:33 +0200

    Test, implemented findAll test, failing

---
+++ src/test/java/dev/justgiulio/passwordmanager/controller/AccountControllerTest.java
@@ -0,0 +1,49 @@
+
+package dev.justgiulio.passwordmanager.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static java.util.Arrays.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import dev.justgiulio.passwordmanager.model.Account;
+import dev.justgiulio.passwordmanager.model.Credential;
+import dev.justgiulio.passwordmanager.repository.AccountRepository;
+import dev.justgiulio.passwordmanager.view.AccountView;
+
+public class AccountControllerTest {
+
+	@Mock
+	private AccountRepository passwordRepository;
+	
+	@Mock
+	private AccountView accountView;
+	
+	@InjectMocks
+	AccountController controller;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void findAllAccountTest() {
+		List<Account> accounts = Arrays.asList(new Account("github.com", new Credential("giulio","giulio")));
+		when(passwordRepository.findAll()).thenReturn(accounts);
+		controller.findAll();
+		verify(accountView).showAllAccounts(accounts);
+	}
+
+}
