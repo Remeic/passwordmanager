f83efff6c2dbe4d88ab46c7006d386480fed6d61
Giulio Fagioli <fagioli.giulio@gmail.com>
2020-07-21 10:41:42 +0200

    E2E, create folder, update pom with new plugin and dependency

---
+++ src/e2e/java/dev/justgiulio/passwordmanager/view/swing/AccountSwingAppE2E.java
@@ -0,0 +1,11 @@
+package dev.justgiulio.passwordmanager.view.swing;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.testcontainers.containers.GenericContainer;
+
+@RunWith(GUITestRunner.class)
+public class AccountSwingAppE2E {
+
+}
